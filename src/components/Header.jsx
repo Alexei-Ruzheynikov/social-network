efa92d38d8ca515bcc0671f11fcc01294ca2b1d2
Alexei-Ruzheunikov <alexei_ruzheynikov@mail.ru>
2022-05-05 19:43:32 +0300

    Init project

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://seeklogo.com/images/C/company-name-logo-09881CAD1A-seeklogo.com.png" />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,20 @@
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://docs.microsoft.com/ru-ru/windows/apps/design/controls/images/image-licorice.jpg" />
+      </div>
+      <div>ava + description</div>
+      <div>
+        My posts
+        <div>New posts</div>
+        <div>
+          <div>post1</div>
+          <div>post1</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
